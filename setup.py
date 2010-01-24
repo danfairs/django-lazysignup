a54be0364f2e7787ea5655778448dd7ba4264dff
Dan Fairs <dan@fezconsulting.com>
2010-01-24 12:09:45 +0000

    Change name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 version = '0.1'
 
-setup(name='lazysignup',
+setup(name='django-lazysignup',
       version=version,
       description="Lazy signup for Django",
       long_description=open("README.txt").read() + "\n" +
