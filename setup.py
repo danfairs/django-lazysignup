11eeee01a2e5f09fbee4135446745d0930fbc4b5
Dan Fairs <dan@fezconsulting.com>
2010-01-27 21:45:57 +0000

    Fix file name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.1'
 setup(name='django-lazysignup',
       version=version,
       description="Lazy signup for Django",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
