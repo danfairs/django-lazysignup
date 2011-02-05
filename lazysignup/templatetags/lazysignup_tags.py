c0c06010908a154b6c8e19b5b52a3ad7fed453c6
Dan Fairs <dan@fezconsulting.com>
2011-02-05 19:28:55 +0000

    Move the is_lazy_user implementation into utils, so it's not necessary to import it from a silly location each time.

--- lazysignup/templatetags/lazysignup_tags.py
+++ lazysignup/templatetags/lazysignup_tags.py
@@ -1,20 +1,5 @@
 from django.template import Library
-from lazysignup.models import LazyUser
+from lazysignup.utils import is_lazy_user
+
 register = Library()
-
-def is_lazy_user(user):
-    """ Return True if the passed user is a lazy user. """
-    # Anonymous users are not lazy.
-    if user.is_anonymous():
-        return False
-
-    # Check the user backend. If the lazy signup backend
-    # authenticated them, then the user is lazy.
-    backend = getattr(user, 'backend', None)
-    if backend == 'lazysignup.backends.LazySignupBackend':
-        return True
-
-    # Otherwise, we have to fall back to checking the database.
-    return bool(LazyUser.objects.filter(user=user).count() > 0)
-
 is_lazy_user = register.filter(is_lazy_user)
--- lazysignup/utils.py
+++ lazysignup/utils.py
@@ -5,3 +5,19 @@ def username_from_session(session_key, username_length=None):
     if not username_length:
         username_length = User._meta.get_field('username').max_length
     return hashlib.sha1(session_key).hexdigest()[:username_length]
+
+def is_lazy_user(user):
+    """ Return True if the passed user is a lazy user. """
+    # Anonymous users are not lazy.
+    if user.is_anonymous():
+        return False
+
+    # Check the user backend. If the lazy signup backend
+    # authenticated them, then the user is lazy.
+    backend = getattr(user, 'backend', None)
+    if backend == 'lazysignup.backends.LazySignupBackend':
+        return True
+
+    # Otherwise, we have to fall back to checking the database.
+    from lazysignup.models import LazyUser
+    return bool(LazyUser.objects.filter(user=user).count() > 0)
