390d4d4de1db33d65e870baa3bf749bb0b6e1d54
Mitar <mitar@tnode.com>
2011-10-31 20:48:45 +0100

    Fixing default URLs.

--- lazysignup/urls.py
+++ lazysignup/urls.py
@@ -4,8 +4,8 @@ from django.views.generic.simple import direct_to_template
 # URL patterns for lazysignup
 
 urlpatterns = patterns('lazysignup.views',
-    url(r'^convert/$', 'convert', name='lazysignup_convert'),
-    url(r'^convert/done/$', direct_to_template, {
+    url(r'^$', 'convert', name='lazysignup_convert'),
+    url(r'^done/$', direct_to_template, {
         'template': 'lazysignup/done.html',
         }, name='lazysignup_convert_done'),
 )
