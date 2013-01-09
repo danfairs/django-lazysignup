5c98932997e86753815ed48cd9e85812c141f494
Ben Standefer <benstandefer@gmail.com>
2013-01-08 23:53:55 -0800

    Fix 'No module named simple' error when using Django 1.5. Could probably be done cleaner, but wanted to keep it backwards-compatible.

--- lazysignup/urls.py
+++ lazysignup/urls.py
@@ -1,11 +1,24 @@
+import django
+print django.VERSION
+django_version = float('%s.%s' % (django.VERSION[0], django.VERSION[1]))
+print django_version
+
 from django.conf.urls.defaults import patterns, url
-from django.views.generic.simple import direct_to_template
 
 # URL patterns for lazysignup
 
-urlpatterns = patterns('lazysignup.views',
-    url(r'^$', 'convert', name='lazysignup_convert'),
-    url(r'^done/$', direct_to_template, {
-        'template': 'lazysignup/done.html',
-        }, name='lazysignup_convert_done'),
-)
+if django_version >= 1.5:
+    from django.views.generic import TemplateView
+    urlpatterns = patterns('lazysignup.views',
+        url(r'^$', 'convert', name='lazysignup_convert'),
+        url(r'^done/$', TemplateView.as_view(template_name="lazysignup/done.html"),
+            name='lazysignup_convert_done'),
+    )
+else:
+    from django.views.generic.simple import direct_to_template
+    urlpatterns = patterns('lazysignup.views',
+        url(r'^$', 'convert', name='lazysignup_convert'),
+        url(r'^done/$', direct_to_template, {
+            'template': 'lazysignup/done.html',
+            }, name='lazysignup_convert_done'),
+    )
