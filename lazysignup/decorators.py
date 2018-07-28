61d7bb99b58a1e1c2fa0d6c1d7678d90be414720
Mathieu <laundro@gmail.com>
2018-07-28 23:09:27 +0200

    Fix typo

--- lazysignup/decorators.py
+++ lazysignup/decorators.py
@@ -17,7 +17,7 @@ ALLOW_LAZY_REGISTRY = {}
 def allow_lazy_user(func):
     def wrapped(request, *args, **kwargs):
         assert hasattr(request, 'session'), ("You need to have the session "
-                                             "app intsalled")
+                                             "app installed")
         if getattr(settings, 'LAZYSIGNUP_ENABLE', True):
             # If the user agent is one we ignore, bail early
             ignore = False
